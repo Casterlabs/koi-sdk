53323dd2d3e381306669f48232b2be81c666335d
Ender <33337309+e3ndr@users.noreply.github.com>
2024-05-01 06:44:29 -0500

    Java: Helper to populate profile info.
    This is secretly getting used on the backend. Shhhh.

--- java/src/main/java/co/casterlabs/koi/api/types/user/SimpleProfile.java
+++ java/src/main/java/co/casterlabs/koi/api/types/user/SimpleProfile.java
@@ -1,10 +1,14 @@
 package co.casterlabs.koi.api.types.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import co.casterlabs.rakurai.json.Rson;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import co.casterlabs.rakurai.json.annotating.JsonField;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
@@ -18,6 +22,11 @@ public class SimpleProfile {
     public final @NonNull String id;
     public final @NonNull @JsonField("channel_id") String channelId;
     public final @NonNull UserPlatform platform;
+
+    /**
+     * This is used internally. Beware of dragons.
+     */
+    public final @Deprecated @Builder.Default Map<String, Object> extraMetadata = new HashMap<>();
 
     public int tryGetIdAsInt() {
         return Integer.parseInt(this.id);
--- java/src/main/java/co/casterlabs/koi/api/types/user/User.java
+++ java/src/main/java/co/casterlabs/koi/api/types/user/User.java
@@ -1,6 +1,8 @@
 package co.casterlabs.koi.api.types.user;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import co.casterlabs.rakurai.json.annotating.JsonField;
@@ -44,8 +46,9 @@ public class User extends SimpleProfile {
     @JsonField("subscriber_count")
     public final @NonNull Long subCount;
 
+    @SuppressWarnings("deprecation")
     public SimpleProfile cloneSimpleProfile() {
-        return new SimpleProfile(this.UPID, this.id, this.channelId, this.platform);
+        return new SimpleProfile(this.UPID, this.id, this.channelId, this.platform, this.extraMetadata);
     }
 
     public static enum UserRoles {
@@ -56,6 +59,80 @@ public class User extends SimpleProfile {
         STAFF,
         VIP,
         OG;
+    }
+
+    /**
+     * Populates the color and pronouns based in intrinsics. Use this to give your
+     * custom integration a more consistent feel.
+     */
+    public static void populateMissingInfo(UserBuilder<?, ?> builder) {
+        if (builder.bio == null) {
+            builder.bio(""); // So the below code doesn't break.
+        }
+
+        if (builder.color == null) {
+            final Pattern PATTERN = Pattern.compile("(\\[color:#[a-f0-9]{6}\\])");
+            final String[] COLORS = new String[] {
+                    "#FF0000",
+                    "#FF8000",
+                    "#FFFF00",
+                    "#80FF00",
+                    "#00FF00",
+                    "#00FF80",
+                    "#00FFFF",
+                    "#0080FF",
+                    "#0000FF",
+                    "#7F00FF",
+                    "#FF00FF",
+                    "#FF007F"
+            };
+
+            // Try to grab a hex code from their bio if they have it.
+            Matcher m = PATTERN.matcher(builder.bio.toLowerCase());
+            if (m.find()) {
+                String group = m.group();
+                String color = group.substring(
+                    group.indexOf(':') + 1,
+                    group.length() - 1
+                ); // [color:#ea4c4c] -> #ea4c4c
+
+                builder.color(color);
+            } else {
+                int hashValue = builder.username.hashCode();
+                int index = Math.abs(hashValue);
+
+                String color = COLORS[index % COLORS.length];
+                builder.color(color);
+            }
+        }
+
+        if (builder.pronouns == null) {
+            final Pattern PATTERN = Pattern.compile("(\\[pronouns:[a-z]+\\])");
+
+            // Try to grab pronouns from their bio if they have it.
+            Matcher m = PATTERN.matcher(builder.bio.toLowerCase());
+            if (m.find()) {
+                String group = m.group();
+                String str = group.substring(
+                    group.indexOf(':') + 1,
+                    group.length() - 1
+                ).toUpperCase(); // [pronouns:he] -> HE
+
+                try {
+                    builder.pronouns(Pronouns.valueOf(str));
+                } catch (IllegalArgumentException ignored) {
+                    // Invalid pronoun, ignore it.
+                    builder.pronouns(Pronouns.UNKNOWN);
+                }
+            } else {
+                builder.pronouns(Pronouns.UNKNOWN);
+            }
+        }
+
+        if (builder.imageLink == null) {
+            final String DEFAULT_PROFILE_PICTURE = "data:image/svg+xml,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%20standalone%3D%22no%22%3F%3E%0A%3Csvg%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22whitesmoke%22%20stroke-width%3D%221.5%22%20version%3D%221.1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Asvg%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%0A%20%20%20%20%3Crect%20style%3D%22fill%3A%23676767%3Bfill-opacity%3A1%3Bstroke%3Anone%3B%22%20width%3D%2224%22%20height%3D%2224%22%20x%3D%220%22%20y%3D%220%22%20%2F%3E%0A%20%20%20%20%3Cpath%20d%3D%22M20%2021v-2a4%204%200%200%200-4-4H8a4%204%200%200%200-4%204v2%22%20%2F%3E%0A%20%20%20%20%3Ccircle%20cx%3D%2212%22%20cy%3D%227%22%20r%3D%224%22%20%2F%3E%0A%3C%2Fsvg%3E%0A";
+            builder.imageLink(DEFAULT_PROFILE_PICTURE);
+        }
     }
 
 }
