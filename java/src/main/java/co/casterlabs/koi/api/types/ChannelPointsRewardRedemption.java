56491fc60656d85267b5955749887a5ede5b39c1
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-05-12 05:31:11 -0500

    Java: Bring ChannelPointsRewardRedemption up-to-spec with the backend.

--- java/src/main/java/co/casterlabs/koi/api/types/ChannelPointsRewardRedemption.java
+++ java/src/main/java/co/casterlabs/koi/api/types/ChannelPointsRewardRedemption.java
@@ -3,8 +3,12 @@ package co.casterlabs.koi.api.types;
 import org.jetbrains.annotations.Nullable;
 
 import co.casterlabs.koi.api.GenericBuilder;
+import co.casterlabs.koi.api.GenericBuilder.BuilderDefault;
 import co.casterlabs.rakurai.json.annotating.JsonClass;
 import co.casterlabs.rakurai.json.annotating.JsonField;
+import co.casterlabs.rakurai.json.annotating.JsonSerializationMethod;
+import co.casterlabs.rakurai.json.element.JsonElement;
+import co.casterlabs.rakurai.json.element.JsonString;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 
@@ -17,25 +21,34 @@ public class ChannelPointsRewardRedemption {
 
     public final @NonNull Integer cost = null;
 
+    @BuilderDefault("null")
     @JsonField("background_color")
-    public final @NonNull String backgroundColor = null;
+    public final @Nullable String backgroundColor = null;
 
+    @BuilderDefault("null")
     @JsonField("reward_image")
-    public final @NonNull String rewardImage = null;
-
-    @JsonField("default_reward_image")
-    public final @NonNull String defaultRewardImage = null;
+    public final @Nullable String rewardImage = null;
 
     /**
      * Null if disabled. See {@link #userInput}
      */
+    @BuilderDefault("null")
     public final @Nullable String prompt = null;
 
     /**
      * Null if disabled. See {@link #prompt}
      */
+    @BuilderDefault("null")
     @JsonField("user_input")
     public final @Nullable String userInput = null;
+
+    /**
+     * Compat.
+     */
+    @JsonSerializationMethod("default_reward_image")
+    private JsonElement $serialize_default_reward_image() {
+        return new JsonString(this.rewardImage);
+    }
 
     public Builder toBuilder() {
         return new Builder(this);
@@ -71,18 +84,13 @@ public class ChannelPointsRewardRedemption {
             return this;
         }
 
-        public Builder backgroundColor(@NonNull String value) {
+        public Builder backgroundColor(@Nullable String value) {
             this.put("backgroundColor", value);
             return this;
         }
 
         public Builder rewardImage(@NonNull String value) {
             this.put("rewardImage", value);
-            return this;
-        }
-
-        public Builder defaultRewardImage(@NonNull String value) {
-            this.put("defaultRewardImage", value);
             return this;
         }
 
